0233ef896b63ef77530c26b7589093bb9a85becf
JEYBAN37 <jeyban37@gmail.com>
2024-07-15 19:13:43 -0500

    Add SMS TWILO

--- src/main/java/com/example/rate/RateApplication.java
+++ src/main/java/com/example/rate/RateApplication.java
@@ -2,12 +2,18 @@ package com.example.rate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+
 
 @SpringBootApplication
 public class RateApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RateApplication.class, args);
+
 	}
 }
 
--- src/main/java/com/example/rate/notify/SmsNotify.java
+++ src/main/java/com/example/rate/notify/SmsNotify.java
@@ -1,7 +1,16 @@
 package com.example.rate.notify;
+import com.twilio.http.TwilioRestClient;
+import org.springframework.stereotype.Service;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+@Service
 public class SmsNotify implements Notification{
+    private TwilioRestClient twilioRestClient;
     @Override
     public void send(String receiver) {
-        System.out.println("Sending SMS with message: " + receiver);
+        Message.creator(
+                new PhoneNumber(receiver),
+                new PhoneNumber("3177722509"),"Hola un Saludo Especial te Invito A visualizar Este Codigo de Aprendisaje"
+        ).create(twilioRestClient);
     }
 }
